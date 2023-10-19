6255bea2bb5c53fd32bf7f44f6f00898eb6f251c
Filipp Druan <druanf@bk.ru>
2023-10-19 20:40:08 +0300

    Solve the second task

---
+++ task-2.lisp
@@ -0,0 +1,40 @@
+(in-package job-tasks-tests)
+
+#| Задание 2
+Опишите функцию трех аргументов l, n, m, где первый аргумент список, n
+— номер уровня, m — номер позиции. Вызов (fn l n m) должен возвращать
+список, полученный из l удалением элемента в позиции m в каждом подсписке
+на уровне n.
+Например, вызов
+(fn '(c (a ((b) a (b)) a) (a d (a) (b (b a (b) a z) a h a a (b) a)a))
+    3
+    3)
+должен возвратить
+(c (a ((b) a) a) (a d (a) (b (b a (b) a z) h a a (b) a) a))
+
+|#
+; Мне не нравится, что номер позиции начинается с единицы. Я привык, что отсчёт идёт с единицы. Сделаем переменным!
+
+
+(defun delete-from-pos (list pos &key (start-pos 0))
+    (loop for i from start-pos
+          for elm in list
+          unless (= i pos)
+            collect elm))
+
+(defun delete-from-pos-on-level (list pos level &key (start-pos 0) (start-level 1))
+    (if (= level start-level)
+        (delete-from-pos list pos)
+        (loop for elm in list
+              collect (delete-from-pos-on-level list
+                                                pos
+                                                (1- level)
+                                                :start-pos start-pos
+                                                :start-level start-level))))
+
+(defun task-2 (list pos level)
+    (delete-from-pos-on-level list
+                              pos
+                              level
+                              :start-pos 1
+                              :start-level 1))
--- tests.lisp
+++ tests.lisp
@@ -1,4 +1,4 @@
-(in-package job-tasks-test)
+(in-package job-tasks-tests)
 
 (define-test c*r-search
     (is equal
@@ -15,3 +15,43 @@
         '(car car cdr car cdr)
         (c*r-search '(B ((C) (A)))
                     'A)))
+
+
+(defun dfp (list pos)
+    (delete-from-pos list pos))
+
+(defun dfp-1 (list pos)
+    (delete-from-pos list pos :start-pos 1))
+
+(define-test delete-from-pos
+    (is equal
+        '(1 2 3 4 5)
+        (dfp '(1 2 3 4 5)
+             1234))
+    
+    (is equal
+        '(2 3 4 5)
+        (dfp '(1 2 3 4 5)
+             0))
+
+    (is equal
+        '(1 3 4 5)
+        (dfp '(1 2 3 4 5)
+             1))
+
+    (is equal
+        '(1 3 4 5)
+        (dfp-1 '(1 2 3 4 5)
+               2))
+
+    (is equal
+        '(2 3 4 5)
+        (dfp-1 '(1 2 3 4 5)
+               1)))
+
+(define-test test-2
+    (is equal
+        '(c (a ((b) a) a) (a d (a) (b (b a (b) a z) h a a (b) a) a))
+        (foo '(c (a ((b) a (b)) a) (a d (a) (b (b a (b) a z) a h a a (b) a)a))
+             3
+             3)))
