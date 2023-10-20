cfd0cc73d772ae6caac920d1e2bae1bbb0966c12
Filipp Druan <druanf@bk.ru>
2023-10-20 20:46:50 +0300

    Solve the task-1 in general form

--- task-1.lisp
+++ task-1.lisp
@@ -11,3 +11,24 @@
 (car (car (car (car (cdr (cdr (cdr (car (cdr (cdr (car *lst*)))))))))))
 
 ; Найдём общее решение: напишем функцию, которая принимает дерево cons-ячеек и объект, путь к которому нужно найти, после чего находит последовательность символов car и cdr, которая служит путём к искомому объекту.
+
+
+
+(defun c*r-search (tree elm)
+    (cond ((eql tree elm)
+           nil)
+
+          ((consp tree)
+           (let ((path))
+               (setf path (c*r-search (car tree)
+                                      elm))
+               (if (eql path :not-found)
+                   (setf path (c*r-search (cdr tree)
+                                          elm))
+                   (return-from c*r-search (cons 'car path)))
+
+               (if (eql path :not-found)
+                   (return-from c*r-search :not-found)
+                   (return-from c*r-search (cons 'cdr path)))))
+          (t
+           :not-found)))
