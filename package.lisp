2da4863b62cad0ac81f0b143541ae2b553fb3a05
Filipp Druan <druanf@bk.ru>
2023-10-21 17:37:52 +0300

    Export symbols for task-3.lisp

--- package.lisp
+++ package.lisp
@@ -9,7 +9,11 @@
            ; Task 2
            :delete-from-pos
            :delete-from-pos-on-level
-           :task-2))
+           :task-2
+           ; Task 3
+           :do-range
+           :S-etalon
+           :task-3))
 
 (defpackage :job-tasks-tests
   (:use :cl :parachute :job-tasks))
